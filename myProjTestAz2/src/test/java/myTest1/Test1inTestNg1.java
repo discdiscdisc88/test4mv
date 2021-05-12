766075cd75b325345429755ac9072075f0035591
mint <mint@mint>
2021-05-12 18:41:33 +0200

    1st commit mv4

---
+++ myProjTestAz2/src/test/java/myTest1/Test1inTestNg1.java
@@ -0,0 +1,58 @@
+package myTest1;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.AssertJUnit;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+
+public class Test1inTestNg1 {
+	WebDriver driver;
+
+	@BeforeMethod
+	public void setUp() throws Exception {
+	
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	
+	}
+	
+	@Test
+	public void test1() throws Exception {
+
+		driver.navigate().to("http://google.com");
+		System.out.println("Test2 "+driver.getTitle());
+		AssertJUnit.assertEquals(driver.getTitle(), "Google");
+
+	}
+
+	@Test
+	public void test2() throws Exception {
+
+		driver.navigate().to("http://demo.guru99.com/test/newtours/register.php");
+		System.out.println("Test2 "+driver.getTitle());
+		AssertJUnit.assertEquals(driver.getTitle(), "Register: Mercury Tours");
+
+	}
+	
+	@AfterMethod
+	public void tearDown() throws Exception {
+		driver.manage().deleteAllCookies();
+		Thread.sleep(2000);
+		driver.quit();
+		System.out.println("Exit");
+	}
+
+}
